381bc7713fbbfc157a6194bcb2d1af4f6c6fbbe4
shsha0110 <shsh0201@snu.ac.kr>
2025-09-25 15:04:26 +0900

    build: Migrate project to Vite environment

--- netflix-clone/src/main.js
+++ netflix-clone/src/main.js
@@ -1,3 +1,5 @@
+import './style.css'; 
+
 import { create_header } from "./header/header.js";
 import { create_hero } from "./hero/hero.js";
 import { create_content } from "./content/content.js";
@@ -7,3 +9,4 @@ create_header(document.body);
 create_hero(document.body);
 create_content(document.body);
 create_footer(document.body);
+
