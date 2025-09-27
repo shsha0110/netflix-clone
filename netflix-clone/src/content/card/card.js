284e2e9afb7ab484236b733ddc8dfe7db292ba99
shsha0110 <shsh0201@snu.ac.kr>
2025-09-27 10:48:33 +0900

    feat(card): Implement hover tooltips for inner card buttons

--- netflix-clone/src/content/card/card.js
+++ netflix-clone/src/content/card/card.js
@@ -76,65 +76,101 @@ export class Card {
     create_opinion_modal(parent) {
         this.opinion_button = create_component_with_img("button", "content-opinion-button", parent, this.icon_data.like_button);
         this.opinion_modal_container = create_component("div", "content-opinion-modal-container", this.opinion_button);
-        this.dislike_button = create_component_with_img("button", "content-dislike-button", this.opinion_modal_container, this.icon_data.dislike_button);
-        this.like_button = create_component_with_img("button", "content-like-button",  this.opinion_modal_container, this.icon_data.like_button);
-        this.best_button = create_component_with_img("button", "content-best-button", this.opinion_modal_container, this.icon_data.best_button);
 
+        this.create_dislike(this.opinion_modal_container);
+        this.create_like(this.opinion_modal_container);
+        this.create_best(this.opinion_modal_container);
+        
+        this.update_opinion_buttons(null);
+    }
+
+    create_dislike(parent) {
+        this.dislike_button = create_component_with_img("button", "content-dislike-button", parent, this.icon_data.dislike_button);
         this.dislike_button.addEventListener("click", () => {
-            this.update_opinion_button_icons("dislike");
+            this.update_opinion_buttons("dislike");
             this.set_user_opinion("dislike");
         });
 
+        this.dislike_tooltip = create_component("div", "content-dislike-tooltip", this.dislike_button);
+        this.dislike_tooltip_text = create_component("span", "content-tooltip-text", this.dislike_tooltip);
+        this.dislike_tooltip_text.textContent = "맘에 안 들어요";
+        this.dislike_tooltip_arrow = create_component("div", "content-tooltip-arrow", this.dislike_tooltip);
+    }
+
+    create_like(parent) {
+        this.like_button = create_component_with_img("button", "content-like-button",  parent, this.icon_data.like_button);
         this.like_button.addEventListener("click", () => {
-            this.update_opinion_button_icons("like");
+            this.update_opinion_buttons("like");
             this.set_user_opinion("like");
         });
 
+        this.like_tooltip = create_component("div", "content-like-tooltip", this.like_button);
+        this.like_tooltip_text = create_component("span", "content-tooltip-text", this.like_tooltip);
+        this.like_tooltip_text.textContent = "좋아요";
+        this.like_tooltip_arrow = create_component("div", "content-tooltip-arrow", this.like_tooltip);
+    }
+
+    create_best(parent) {
+        this.best_button = create_component_with_img("button", "content-best-button", parent, this.icon_data.best_button);
         this.best_button.addEventListener("click", () => {
-            this.update_opinion_button_icons("best");
+            this.update_opinion_buttons("best");
             this.set_user_opinion("best");
         });
 
-        this.update_opinion_button_icons(null);
+        this.best_tooltip = create_component("div", "content-best-tooltip", this.best_button);
+
+        this.best_tooltip_text = create_component("span", "content-tooltip-text", this.best_tooltip);
+        this.best_tooltip_text.textContent = "최고예요!";
+
+        this.best_tooltip_arrow = create_component("div", "content-tooltip-arrow", this.best_tooltip);
     }
 
-    update_opinion_button_icons(opinion) {
-        this.reset_opinion_button_icons();
+    update_opinion_buttons(opinion) {
+        this.reset_opinion_buttons();
         const user_opinion = this.get_user_opinion();
         if (opinion) {
             if (user_opinion != opinion) {
                 if (opinion === "dislike") {
                     this.dislike_button.querySelector("img").src = this.icon_data.dislike_button_active;
                     this.opinion_button.querySelector("img").src = this.icon_data.dislike_button_active;
+                    this.dislike_tooltip_text.textContent = "평가함";
                 } else if (opinion === "like") {
                     this.like_button.querySelector("img").src = this.icon_data.like_button_active;
                     this.opinion_button.querySelector("img").src = this.icon_data.like_button_active;
+                    this.like_tooltip_text.textContent = "평가함";
                 } else if (opinion === "best") {
                     this.best_button.querySelector("img").src = this.icon_data.best_button_active;
                     this.opinion_button.querySelector("img").src = this.icon_data.best_button_active;
+                    this.best_tooltip_text.textContent = "평가함";
                 }   
             } else {
-                this.reset_opinion_button_icons();
+                this.reset_opinion_buttons();
             }
         } else {
-            if (user_opinion === "dislike") {
-                this.dislike_button.querySelector("img").src = this.icon_data.dislike_button_active;
-                this.opinion_button.querySelector("img").src = this.icon_data.dislike_button_active;
-            } else if (user_opinion === "like") {
-                this.like_button.querySelector("img").src = this.icon_data.like_button_active;
-                this.opinion_button.querySelector("img").src = this.icon_data.like_button_active;
-            } else if (user_opinion === "best") {
-                this.best_button.querySelector("img").src = this.icon_data.best_button_active;
-                this.opinion_button.querySelector("img").src = this.icon_data.best_button_active;
-            }
+            if (opinion === "dislike") {
+                    this.dislike_button.querySelector("img").src = this.icon_data.dislike_button_active;
+                    this.opinion_button.querySelector("img").src = this.icon_data.dislike_button_active;
+                    this.dislike_tooltip_text.textContent = "평가함";
+                } else if (opinion === "like") {
+                    this.like_button.querySelector("img").src = this.icon_data.like_button_active;
+                    this.opinion_button.querySelector("img").src = this.icon_data.like_button_active;
+                    this.like_tooltip_text.textContent = "평가함";
+                } else if (opinion === "best") {
+                    this.best_button.querySelector("img").src = this.icon_data.best_button_active;
+                    this.opinion_button.querySelector("img").src = this.icon_data.best_button_active;
+                    this.best_tooltip_text.textContent = "평가함";
+                }
         }
     }
 
-    reset_opinion_button_icons() {
+    reset_opinion_buttons() {
         this.opinion_button.querySelector("img").src = this.icon_data.like_button;
         this.dislike_button.querySelector("img").src = this.icon_data.dislike_button;
         this.like_button.querySelector("img").src = this.icon_data.like_button;
         this.best_button.querySelector("img").src = this.icon_data.best_button;
+        this.dislike_tooltip_text.textContent = "맘에 안 들어요";
+        this.like_tooltip_text.textContent = "좋아요";
+        this.best_tooltip_text.textContent = "최고예요!";
     }
 
     set_user_opinion(opinion) {
